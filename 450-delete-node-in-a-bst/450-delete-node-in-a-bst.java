52b0a74db244f845050cdce65226409a97637e0b
Mujeeb02 <83205006+Mujeeb02@users.noreply.github.com>
2022-09-16 16:41:57 +0530

    Time: 0 ms (100.00%), Space: 49.7 MB (44.27%) - LeetHub

---
+++ 450-delete-node-in-a-bst/450-delete-node-in-a-bst.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root==null){
+            return null;
+        }
+        if(root.val<key){
+            root.right=deleteNode(root.right,key);
+        }
+        else if(root.val>key){
+            root.left=deleteNode(root.left,key);
+        }
+        else{
+            if(root.left==null){
+               return root.right; 
+            }
+            else if(root.right==null){
+                return root.left;
+            }
+            else{
+                TreeNode curr=getnext(root);
+                root.val=curr.val;
+                root.right=deleteNode(root.right,curr.val);
+                
+            }
+        }
+        return root;
+    }
+    TreeNode getnext(TreeNode root){
+        TreeNode curr=root.right;
+        while(curr!=null && curr.left!=null){
+            curr=curr.left;
+        }
+        return curr;
+    }
+}
